8ebc56677f28a5ec9a4c8f3d498252b2c25dfdeb
Mahmoud AlSharif <mahmoud.k.alsharif@gmail.com>
2025-06-11 22:30:16 +0300

    changes

---
+++ notes.js
@@ -0,0 +1,16 @@
+
+// TODO pending.js is not needed Total to be migrated is 3000000
+// successfully migrated 1000/3000000 (2%)
+// successfully migrated 2000/3000000 (2.2%)
+
+
+// TODO summary.js is not needed Total to be migrated is 3000000
+// successfully migrated 30000000/3000000 (99.4%)
+// Errored for 1000/3000000 (0.6%)
+// Errors ids are [ '5f8d0c0b9b1e8c001f8b4567', '5f8d0c0b9b1e8c001f8b4568' ]
+
+//errored_id.json
+//{
+//  "errored_ids": []
+//  "errored_responses": ["cannot read propery for document blablabla"]
+//}
